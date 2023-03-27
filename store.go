a93f53ab98746aae0d66b9b979992e14e8a83e0c
Michael Henry Pantaleon <me@iamkel.net>
2023-03-28 01:37:45 +1100

    prepare generic functions to fetch and save data.

--- store.go
+++ store.go
@@ -2,66 +2,115 @@ package omise
 
 import (
 	"context"
-	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-type Model interface{}
-
-type Store struct {
-	Config aws.Config
-	TableName string
+type Model interface {
+	Pk() string
+	Sk() string
 }
 
-// Create new store
-func NewStore(TableName string) *Store {
+type Store struct {
+	client    *dynamodb.Client
+	tableName string
+}
+
+type awsCreds struct {
+	keyId string
+	secretKey string
+}
+
+func (c *awsCreds)	Retrieve(ctx context.Context) (aws.Credentials, error) {
+	return aws.Credentials{
+		AccessKeyID: c.keyId, SecretAccessKey: c.secretKey,
+	}, nil
+}
+
+func NewStore(TableName string, Region string, KeyId string, SecretKey string) *Store {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), func(o *config.LoadOptions) error {
-		o.Region = os.Getenv("AWS_DEFAULT_REGION")
+		o.Region = Region
+		o.Credentials = &awsCreds{
+			keyId: KeyId,
+			secretKey: SecretKey,
+		}
 		return nil
 	})
+
 	if err != nil {
 		panic(err)
 	}
-	return &Store{cfg, TableName}
+
+  client := dynamodb.NewFromConfig(cfg)
+	return &Store{client, TableName}
 }
 
-// Save an item
-func (s *Store) Save(model Model) (Model, error) {
+func (s *Store) saveItem(model Model) (map[string]types.AttributeValue, error) {
 	av, err := attributevalue.MarshalMap(model)
 	if err != nil {
 		return nil, err
 	}
 
-	client := dynamodb.NewFromConfig(s.Config)
-	_, err = client.PutItem(context.TODO(), &dynamodb.PutItemInput{
-		TableName: aws.String(s.TableName),
+	av["Pk"] = &types.AttributeValueMemberS{Value: model.Pk()}
+	av["Sk"] = &types.AttributeValueMemberS{Value: model.Sk()}
+	out, err := s.client.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String(s.tableName),
 		Item:      av,
 	})
-
-	if err != nil {
-		return nil, err
-	}
-	return model, nil
+	return out.Attributes, err
 }
 
-// Get a list of items
-func (s *Store) Get() ([]Model, error) {
-	client := dynamodb.NewFromConfig(s.Config)
-	results, err := client.Scan(context.TODO(), &dynamodb.ScanInput{
-		TableName: aws.String(s.TableName),
+func (s *Store) getItem(pk string, sk string) (map[string]types.AttributeValue, error) {
+	out, err := s.client.GetItem(context.TODO(), &dynamodb.GetItemInput{
+		TableName: aws.String(s.tableName),
+		Key: map[string]types.AttributeValue{
+			"Pk": &types.AttributeValueMemberS{Value: pk},
+			"Sk": &types.AttributeValueMemberS{Value: sk},
+		},
 	})
-	if err != nil {
-		return nil, err
-	}
+	return out.Item, err
+}
 
-	var records []Model
-	err = attributevalue.UnmarshalListOfMaps(results.Items, &records)
+func (s *Store) getItems(filterExpression string, expressionAttributeValues map[string]types.AttributeValue) ([]map[string]types.AttributeValue, error) {
+	out, err := s.client.Scan(context.TODO(), &dynamodb.ScanInput{
+		TableName:                 &s.tableName,
+		FilterExpression:          aws.String(filterExpression),
+		ExpressionAttributeValues: expressionAttributeValues,
+		Limit:                     aws.Int32(100),
+	})
+	return out.Items, err
+}
+
+// Transform map to model.
+func GetItem[model any](store *Store, pk string, sk string) (*model, error) {
+	out, err := store.getItem(pk, sk)
 	if err != nil {
+		panic(err)
+	}
+	obj := new(model)
+	if err = attributevalue.UnmarshalMap(out, &obj); err != nil {
 		return nil, err
 	}
-	return records, nil
+	return obj, nil
+}
+
+func GetItems[model any](store *Store, filterExpression string, expressionAttributeValues map[string]types.AttributeValue) ([]model, error) {
+	out, err := store.getItems(filterExpression, expressionAttributeValues)
+	if err != nil {
+		panic(err)
+	}
+	objs := new([]model)
+	if err = attributevalue.UnmarshalListOfMaps(out, &objs); err != nil {
+		return nil, err
+	}
+	return *objs, err
+}
+
+func SaveItem(store *Store, model Model) error {
+	_, err := store.saveItem(model)
+	return err
 }
